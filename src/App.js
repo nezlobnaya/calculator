d7b69938b8a55a6cd6933595ea8c4fb2a744d166
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-10-19 22:37:43 -0400

    Added components to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { useState } from 'react';
 import './App.css';
+import Logo from './components/DisplayComponents/Logo';
+import Display from './components/DisplayComponents/Display';
+import Specials from './components/ButtonComponents/SpecialButtons/Specials';
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
+import Operators from './components/ButtonComponents/OperatorButtons/Operators';
+
+
+
 
 function App() {
+  const [display, setDisplay] = useState('')
+
+
   return (
-    <div className="App">
-      
+    <div className="container">
+      <div className = "main-container">
+        <Logo />
+        <Display total={display}/>
+        <div className='buttons-container'>
+          <div className='special-number'>
+            <Specials setDisplay={setDisplay} display={display}/>
+            <Numbers setDisplay={setDisplay} display={display}/>
+          </div>
+          <div className='operators'>
+            <Operators setDisplay={setDisplay} display={display}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
