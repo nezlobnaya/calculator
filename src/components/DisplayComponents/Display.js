1ef256a7582a9b04a66e59e58efde93c97a31e48
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-10-20 20:27:41 -0400

    minor refactoring

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Display = ({ total }) => {
-  return <div className="display">
-  <div className='displayDigit'>{ total }</div>
-  </div>;
+  return (
+    <div className="display">
+      <div className='displayDigit'>{ total }</div>
+  </div>
+  ) 
 };
 
 export default Display;
